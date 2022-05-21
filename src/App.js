91a8870e27236885f1944351b594037cedf28dd3
gerardodelascuevas <gerdlcp@gmail.com>
2022-05-21 16:36:40 -0500

    Header and react router dom ready for next sprint

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './Components/header';
+import { Route, Routes } from 'react-router-dom';
+import NoMatchRoute from './Components/noMatchRoute';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+       <Header />
+
+      <Routes>
+        <Route index element = { <Counter /> } /> 
+        <Route path='*' element = { <NoMatchRoute /> } /> 
+      </Routes>
    
     </div>
   );
