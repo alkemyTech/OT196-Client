656b3eced60296879c9f533d9343228017b165ac
gerardodelascuevas <gerdlcp@gmail.com>
2022-05-27 18:27:27 -0500

    añadiendo endpoint de eliminacion de usuario

--- src/app/slice.js
+++ src/app/slice.js
@@ -14,22 +14,27 @@ const loginSlice = createSlice({
     }
 })
 
-const REACT_APP_BACKEND = `http://localhost3000`
+const REACT_APP_BACKEND = `http://localhost:3000`
 
-const isMyUserLogged = (user)=> {
+const isMyUserLogged = (userId)=> {
     return async (dispatch)=> {
         try {
-          const response = await axios.post(`${REACT_APP_BACKEND}/auth/login`, user)          
+          const response = await axios.post(`${REACT_APP_BACKEND}/auth/login`, userId)          
           dispatch(submitUserData(response.data))
-
         } catch (error) {
             console.log(error)
         }      
     }
 }
 
-export const deleteUser = async(email)=> {
-    await axios.delete(`${REACT_APP_BACKEND}/users/user/${email}`)
+export const deleteUser = (id)=> {  //FUNCTION TO DELETET USER BY ID 
+    return async function(dispatch){
+        try {
+             await axios.delete(`${REACT_APP_BACKEND}/users/user/${id}`)
+        } catch (error) {
+            console.log(error)
+        }         
+    }    
 }
 
 export  { loginSlice, isMyUserLogged }
