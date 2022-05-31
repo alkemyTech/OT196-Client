ee4c73cdc52d0b445e63e6d5d46bb153783c3a2d
Eliecer Lemus <lemuselr@gmail.com>
2022-05-31 14:27:38 -0400

    [add] Navigation links to navbar

--- src/App.js
+++ src/App.js
@@ -6,24 +6,34 @@ import {
 } from "react-router-dom";
 import './App.css';
 import Home from './components/home/Home';
-import Header from './Components/header';
-import Login from './Components/Login';
+import Header from './components/header';
+import Login from './components/Login';
 import Footer from './components/footer/Footer';
-import NoMatchRoute from './Components/noMatchRoute';
+import NoMatchRoute from './components/noMatchRoute';
+import CreateUser from './components/register/CreateUser'
+import ShowNews from './components/news/ShowNews';
+import Formcontact from './components/formContact';
+
 
 function App() {
   return (
     <div className="App">
       
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
         <Header />
         <Routes>
           <Route index element = { <Home /> } /> 
-          <Route path='/login' element = { <Login /> } /> 
+          <Route path='/login' element = { <Login /> } />
+          <Route path='/signup' element = { <CreateUser /> } />
+          <Route path='/news' element = { <ShowNews /> } />
+          <Route path='/contact-us' element = { <Formcontact /> } />
+          <Route path='/testimonials' element = { <h1>testimonials</h1> } />
+          <Route path='/about-us' element = { <h1>About Us</h1> } />
+          <Route path='/donations' element = { <h1>Donations</h1> } />
           <Route path='*' element = { <NoMatchRoute /> } /> 
         </Routes>
         <Footer/>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </div>
   );
 }
