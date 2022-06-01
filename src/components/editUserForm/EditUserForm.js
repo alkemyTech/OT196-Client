3491dc8a229db043c2e58e527f3b8b42b9c1b5d4
Pedro <pedrobraude@gmail.com>
2022-06-01 08:03:13 -0300

    Fixed useEffect && add state.map

--- src/components/editUserForm/EditUserForm.js
+++ src/components/editUserForm/EditUserForm.js
@@ -9,7 +9,7 @@ import { useState, useEffect } from "react";
 
 export default function EditUserForm() {
 
-  const [user, setUser] = useState()
+  const [user, setUser] = useState([])
 
   const REACT_APP_BACKEND = `http://localhost:3001`
 
@@ -18,45 +18,39 @@ export default function EditUserForm() {
       const res = await axios
         .get(`${REACT_APP_BACKEND}/users`)
         .then(res => {
-          setUser(res.data)})
-    }
+          return (res.data)})
+        .then(response => {setUser(response)})
+          }
     getUsers()
   },[REACT_APP_BACKEND])
-
-  
-
-
-      
-
+console.log(user)
 
   return (
-    <Form >
-      {/* <Form.Group className="mb-3" controlId="formBasicSelect">
-        <Form.Select className='text-center' aria-label="Default select example">
-          <option>Seleccioná usuario a modificar</option>
-          <option value="1">{users[0].name}</option>
-          <option value="2">{users[1].name}</option>
-          <option value="3">{users[2].name}</option>
-        </Form.Select>
-      </Form.Group> */}
-      <Form.Group className="mb-3 " controlId="formBasicName">
-        <Form.Label>Name</Form.Label>
-        <Form.Control className='text-center' type="text" placeholder="Ingresá tu nombre" />
-      </Form.Group>
+      <div>
+      {user.map((item) => (
+        <Form >
+        <Form.Group className="mb-3 " controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control className='text-center' type="text" placeholder={item.firstName} />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicLastName">
-        <Form.Label>Last Name</Form.Label>
-        <Form.Control className='text-center' type="text" placeholder="Ingresá tu apellido" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicLastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control className='text-center' type="text" placeholder={item.lastName} />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicRol">
-        <Form.Label>Rol del Usuario</Form.Label>
-        <Form.Control className='text-center' type="text" placeholder="Elegí tu rol" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicRol">
+          <Form.Label>Rol del Usuario</Form.Label>
+          <Form.Control className='text-center' type="text" placeholder={item.roleId} />
+        </Form.Group>
 
-      <Button variant="primary" type="submit" >
-        Actualizar
-      </Button>
-    </Form>
+        <Button variant="primary" type="submit" >
+          Actualizar
+        </Button>
+      </Form>
+        ))}
+        
+      </div>
+
   );
 }
