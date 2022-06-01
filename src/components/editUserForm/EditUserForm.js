a2ff10e49ae486ed8796b59b375860b17314e6c9
Pedro <pedrobraude@gmail.com>
2022-06-01 11:07:08 -0300

    Add Validation Form

--- src/components/editUserForm/EditUserForm.js
+++ src/components/editUserForm/EditUserForm.js
@@ -2,10 +2,13 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from 'axios'
-
+import Validation from './ValidationForm'
 import { useState, useEffect } from "react";
 
+
 export default function EditUserForm() {
+
+  const [validated, setValidated] = useState(false);
 
   // States for Get User and for the Inputs.
   const [user, setUser] = useState([])
@@ -22,8 +25,9 @@ export default function EditUserForm() {
 
   useEffect(() => {
     async function getUsers() {
+      // eslint-disable-next-line
       const res = await axios
-        .get(`${REACT_APP_BACKEND}/users`)
+        .get(`${REACT_APP_BACKEND}/users/2`)
         .then(res => {
           return (res.data)
         })
@@ -45,43 +49,73 @@ export default function EditUserForm() {
     })
   }
 
-  function submitHandler(e) {
-    e.preventDefault()
-    axios
-      .put(`${REACT_APP_BACKEND}/users/2`, { firstName:userData.firstName, lastName:userData.lastName, roleId:userData.roleId})
-      .then(res => {
-        console.log('success')
-      })
-      .catch((error) => { console.log(error) })
+  // PUT && Submit Handler
+
+  function onSubmit(event) {
+    event.preventDefault()
+    if (validated === false) {
+      Validation(validated,setValidated,event) // Validation Form
+    } else {
+      axios
+        .put(`${REACT_APP_BACKEND}/users/2`, { firstName: userData.firstName, lastName: userData.lastName, roleId: userData.roleId })
+        .then(res => {
+          console.log('success')
+        })
+        .catch((error) => { console.log(error) })
+
     }
+    
+    
+  }
 
+  return (
+    <Form noValidate validated={validated} onSubmit={onSubmit}>
+      <Form.Group className="mb-3 " controlId="formBasicName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          required
+          type="text"
+          name='firstName'
+          onChange={handleChange}
+          value={userData.firstName}
+          className='text-center'
+          placeholder={user.firstName}
+          isInvalid={false}
+        />
+        <Form.Control.Feedback type="invalid">Campo Incompleto</Form.Control.Feedback>
+      </Form.Group>
 
+      <Form.Group className="mb-3" controlId="formBasicLastName">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control
+          required
+          onChange={handleChange}
+          value={userData.lastName}
+          name='lastName'
+          className='text-center'
+          type="text"
+          placeholder={user.lastName}
+        />
+        <Form.Control.Feedback type="invalid">Campo Incompleto</Form.Control.Feedback>
+      </Form.Group>
 
-return (
-  <div>
-      <Form >
-        <Form.Group className="mb-3 " controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" name='firstName' onChange={handleChange} value={userData.firstName} className='text-center' placeholder={user.map(e=>{
-            e.firstName
-          })} />
-        </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicRol">
+        <Form.Label>Rol del Usuario</Form.Label>
+        <Form.Control
+          required
+          onChange={handleChange}
+          value={userData.roleId}
+          name='roleId'
+          className='text-center'
+          type="text"
+          placeholder={user.roleId}
+        />
+        <Form.Control.Feedback type="invalid">Campo Incompleto</Form.Control.Feedback>
+      </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicLastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control onChange={handleChange} value={userData.lastName} name='lastName' className='text-center' type="text" placeholder={user[0]} />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicRol">
-          <Form.Label>Rol del Usuario</Form.Label>
-          <Form.Control onChange={handleChange} value={userData.roleId} name='roleId' className='text-center' type="text" placeholder={user[0]} />
-        </Form.Group>
-
-        <Button variant="primary" type="submit" onClick={submitHandler}>
-          Actualizar
-        </Button>
-      </Form>
-  </div>
-
-);
+      <Button variant="primary" type="submit" >
+        Actualizar
+      </Button>
+    </Form>
+  );
 }
---
+++ src/components/editUserForm/ValidationForm.js
@@ -0,0 +1,12 @@
+
+import { useState } from "react";
+
+export default function Validation(state,setState,event){
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setState(true);
+}
