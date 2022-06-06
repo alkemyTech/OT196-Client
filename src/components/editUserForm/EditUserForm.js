eb0f5909b1b1b03ed4395e317fa1b44e20fd5b45
Pedro <pedrobraude@gmail.com>
2022-06-06 15:55:30 -0300

    Fetch id from store reducer

--- src/components/editUserForm/EditUserForm.js
+++ src/components/editUserForm/EditUserForm.js
@@ -1,114 +1,144 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import axios from 'axios'
-import Validation from './ValidationForm'
+import axios from "axios";
+import Validation from "./ValidationForm";
 import { useState, useEffect } from "react";
+import store from '../../reducers/store'; // Import the store
 
 export default function EditUserForm() {
+
+
+
   // States for validation
   const [validated, setValidated] = useState(false);
 
   // States for Get User and for the Inputs.
-  const [user, setUser] = useState([])
+  const [user, setUser] = useState([]);
   const [userData, setUserData] = useState({
     firstName: "",
     lastName: "",
     roleId: "",
-  })
+  });
+
+
 
   // Temporary Backend PORT
-  const REACT_APP_BACKEND = `http://localhost:3001`
+  const REACT_APP_BACKEND = `http://localhost:3001`;
 
   // Inputs updates
   function handleChange(e) {
-    e.preventDefault()
-    const { name, value } = e.target
-    setUserData(prevState => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setUserData((prevState) => {
       return {
         ...prevState,
-        [name]: value
-      }
-    })
+        [name]: value,
+      };
+    });
   }
+  // Get state from store for use the id
+    
+  const state = store.getState()
+  const id = state.auth.authData.id
+
   // PUT && Submit Handler. For test put a number instead of :id
   function onSubmit(event) {
-    event.preventDefault()
-  
-      Validation(validated,setValidated,event) // Validation Form
-      
-        axios
-          //  PENDING EXACT ROUTER BY  OT196-91 OR OT196-129
-          .put(`${REACT_APP_BACKEND}/users/:id`, { firstName: userData.firstName, lastName: userData.lastName, roleId: userData.roleId })
-          .then(res => {
-            console.log('success')
-          })
-          .catch((error) => { console.log(error) })
-      
+    
+    Validation(validated, setValidated, event); // Validation Form
 
+    axios
+      //  PENDING EXACT ROUTER BY  OT196-91 OR OT196-129
+      .put(`${REACT_APP_BACKEND}/users/${id}`, {
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        roleId: userData.roleId,
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  // Example GET User for placeholders. For test put a number instead of :id
-    useEffect(() => {
-      async function getUsers() {
-        // eslint-disable-next-line
-        const res = await axios
-          .get(`${REACT_APP_BACKEND}/users/:id`)
-          .then(res => {
-            return (res.data)
-          })
-          .then(response => { setUser(response) })
-      }
-      getUsers()
-  
-    }, [REACT_APP_BACKEND])
-  
+  // Example GET User for placeholders. 
+  useEffect(() => {
+    async function getUsers() {
+      // eslint-disable-next-line
+      const res = await axios
+        .get(`${REACT_APP_BACKEND}/users`)
+        .then((res) => {
+          return res.data;
+        })
+        .then((response) => {
+          setUser(response);
+        });
+    }
+    getUsers();
+  }, [REACT_APP_BACKEND]);
+
   return (
-    <Form  className= 'm-4 p-4 border' noValidate validated={validated} onSubmit={onSubmit}>
+    <Form
+      className="m-4 p-4 border"
+      noValidate
+      validated={validated}
+      onSubmit={onSubmit}
+    >
+      {/* <Form.Label className='h4'>Seleccioná el elemento a modificar</Form.Label>
+      <Form.Select className='text-center mb-2'  name='id' aria-label="Default select example">
+        {user.map(e => <option key={e.id}> {e.firstName +' '+ e.lastName}</option>)}
+      </Form.Select> */}
+
       <Form.Group className="mb-3 " controlId="formBasicName">
-        <Form.Label>Name</Form.Label>
+        <Form.Label className='h4'>Nombre</Form.Label>
         <Form.Control
           required
           type="text"
-          name='firstName'
+          name="firstName"
           onChange={handleChange}
           value={userData.firstName}
-          className='text-center'
+          className="text-center"
           placeholder={user.firstName}
           isInvalid={false}
         />
-        <Form.Control.Feedback type="invalid">Campo Incompleto</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Campo Incompleto
+        </Form.Control.Feedback>
       </Form.Group>
 
+
+
       <Form.Group className="mb-3" controlId="formBasicLastName">
-        <Form.Label>Last Name</Form.Label>
+        <Form.Label className='h4'>Apellido</Form.Label>
         <Form.Control
           required
           onChange={handleChange}
           value={userData.lastName}
-          name='lastName'
-          className='text-center'
+          name="lastName"
+          className="text-center"
           type="text"
           placeholder={user.lastName}
         />
-        <Form.Control.Feedback type="invalid">Campo Incompleto</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Campo Incompleto
+        </Form.Control.Feedback>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicRol">
-        <Form.Label>Rol del Usuario</Form.Label>
+        <Form.Label className='h4'>Rol del Usuario</Form.Label>
         <Form.Control
           required
           onChange={handleChange}
           value={userData.roleId}
-          name='roleId'
-          className='text-center'
+          name="roleId"
+          className="text-center"
           type="text"
           placeholder={user.roleId}
         />
-        <Form.Control.Feedback type="invalid">Campo Incompleto</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Campo Incompleto
+        </Form.Control.Feedback>
       </Form.Group>
 
-      <Button variant="primary" type="submit" >
+      <Button variant="primary" type="submit">
         Actualizar
       </Button>
     </Form>
