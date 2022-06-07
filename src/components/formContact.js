1df2ad8108c359063f421cecbe735e67e58b80dc
Pedro <pedrobraude@gmail.com>
2022-06-07 10:27:44 -0300

    Add axios post for connect with endpoint

--- src/components/formContact.js
+++ src/components/formContact.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as yup from 'yup';
 import 'bootstrap/dist/css/bootstrap.css';
-import {axios} from 'axios';
+import  axios  from 'axios';
 
 
 export default function Formcontact() {
+
   // Temporary Backend PORT
   const REACT_APP_BACKEND = `http://localhost:3001`
 
@@ -19,34 +20,35 @@ export default function Formcontact() {
       .email('Este email no es válido')
       .required('Email es un campo obligatorio.')
       .matches(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-      'El Email solo puede contener letras, numeros, puntos, guiones y guion bajo'),
+        'El Email solo puede contener letras, numeros, puntos, guiones y guion bajo'),
     message: yup.string()
       .min(20, 'El mensaje debe contener al menos 20 caracteres')
       .max(300, 'El mensaje no puede contener más de 300 caracteres')
       .required('Mensaje es un campo obligatorio.')
-    });
-  return(
+  });
+  return (
     <div>
       <Formik
-    
+
         initialValues={{
           name: '',
-          email:'',
+          email: '',
           message: ''
         }}
 
         //Validación del schema creado con yup
-        validationSchema= {contactSchema}
+        validationSchema={contactSchema}
 
-                //Función para enviar los valores al endpoint(cuando este exista)
+        //Función para enviar los valores al endpoint(cuando este exista)
         onSubmit={(values, actions) => {
-          setWasSent(true)
+        // Feature: Send data to backend
+        setWasSent(true)
           try {
             axios.post(`${REACT_APP_BACKEND}/contacts`, {
               name: values.name,
               email: values.email,
               message: values.message
-            })            
+            })
           } catch (error) {
             console.log(error)
           }
@@ -54,40 +56,41 @@ export default function Formcontact() {
           actions.resetForm({
             values: {
               name: '',
-              email:'',
+              email: '',
               message: ''
-            }},
+            }
+          },
           )
         }}
-        >
-        {( {errors} ) => (
-			    <Form>
-				    <div className='form-inline'>
-					    <label className="sr-only" htmlFor="name">Nombre</label>
-					    <Field
+      >
+        {({ errors }) => (
+          <Form>
+            <div className='form-inline'>
+              <label className="sr-only" htmlFor="name">Nombre</label>
+              <Field
                 className="form-control"
-					    	type="text"
-					     	name="name"
-					    	placeholder="Ej: Samuel Jackson"
-					    	id="name"
-					    />
+                type="text"
+                name="name"
+                placeholder="Ej: Samuel Jackson"
+                id="name"
+              />
               <ErrorMessage name='name' component={() => (
                 <div className="alert alert-danger">{errors.name}</div>
-              )}/>
-				    </div>
-				    <div className='form-group'>
-					    <label className="sr-only" htmlFor="email">Email de contacto</label>
-					    <Field
+              )} />
+            </div>
+            <div className='form-group'>
+              <label className="sr-only" htmlFor="email">Email de contacto</label>
+              <Field
                 className="form-control"
-					    	type="text"
-					    	name="email"
-					    	placeholder="Ej: Samuel_Jackson@gmail.com"
-					    	id="email"
-				  	  />
+                type="text"
+                name="email"
+                placeholder="Ej: Samuel_Jackson@gmail.com"
+                id="email"
+              />
               <ErrorMessage name='email' component={() => (
                 <div className="alert alert-danger">{errors.email}</div>
-              )}/>
-			  	  </div>
+              )} />
+            </div>
             <div className='form-group'>
               <label className="sr-only">Mensaje</label>
               <Field
@@ -99,12 +102,12 @@ export default function Formcontact() {
               />
               <ErrorMessage name='message' component={() => (
                 <div className="alert alert-danger">{errors.message}</div>
-              )}/>
+              )} />
             </div>
-            <br/>
-				    <button className="btn btn-outline-dark" type="submit">Enviar</button>
+            <br />
+            <button className="btn btn-outline-dark" type="submit">Enviar</button>
             {wasSent ? <div>Enviado con éxito</div> : ''}
-			    </Form>
+          </Form>
         )}
       </Formik>
     </div>
