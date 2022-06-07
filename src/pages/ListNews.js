65890d4c378013aecfa211879e5faabc95bd411a
Facundo <facugastonbarral@gmail.com>
2022-06-07 16:19:16 -0300

    Updating path to novedades

--- src/pages/ListNews.js
+++ src/pages/ListNews.js
@@ -8,7 +8,7 @@ function ListNews() {
     <>
         <Breadcrumb className="mt-3 ms-3">
             <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/" }}>Inicio</Breadcrumb.Item>
-            <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/news" }}>Novedades</Breadcrumb.Item>
+            <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/novedades" }}>Novedades</Breadcrumb.Item>
         </Breadcrumb>
         <Container className="my-5">
             <ShowNews/>
