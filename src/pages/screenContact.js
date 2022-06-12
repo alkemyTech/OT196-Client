0f46cef7b23726fca53bdc7af0b77c60c49d5ec6
Facundo <facugastonbarral@gmail.com>
2022-06-12 01:12:50 -0300

    Modifing styles & columns size

--- src/pages/screenContact.js
+++ src/pages/screenContact.js
@@ -1,16 +1,17 @@
 import React from "react";
-import Formcontact from "../Components/formContact";
+import Formcontact from "../components/formContact";
 import 'bootstrap/dist/css/bootstrap.css';
 
 function ScreenContact(){
     return(
         <div className="container-fluid d-flex mt-3">
             <div className="row justify-content-around">
-                <div className="col-11 col-sm-5 mb-3 border border-dark rounded">
+                <div className="col-11 col-md-5 col-xl-4 mb-3 border border-dark rounded px-0">
                     <h2 className="p-2 bg-dark text-white">
                         Nuestra visión
                     </h2>
-                    <p className='fs-6 m-2'>
+                    
+                    <p className='fs-6 m-2 px-3'>
                         Mejorar la calidad de vida de niños y
                         familias en situación de vulnerabilidad
                         en el barrio La Cava, otorgando un cambio
@@ -21,18 +22,20 @@ function ScreenContact(){
                     <h2 className="p-2 bg-dark text-white">
                         Nuestra misión
                     </h2>
-                    <p className='fs-6 m-2'>
+                    <p className='fs-6 m-2 px-3'>
                         Trabajar articuladamente con los distintos aspectos de la vida de las
                         familias, generando espacios de desarrollo personal y familiar,
                         brindando herramientas que logren mejorar la calidad de vida a
                         través de su propio esfuerzo.
                     </p>
                 </div>
-                <div className="col-11 col-sm-5 mb-3 border border-dark rounded">
+                <div className="col-11 col-md-5 col-xl-4 mb-3 border border-dark rounded px-0">
                     <h2 className="p-2 bg-dark text-white">
                         ¡Contactáte con nosotros!
                     </h2>
+                    <div className="px-3">
                     <Formcontact/>
+                    </div>
                 </div>
             </div>
         </div>
