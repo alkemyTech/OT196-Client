e083ab51f292b8f24085f469fd8a7b2ea0202cbc
Facundo <facugastonbarral@gmail.com>
2022-06-13 23:04:34 -0300

    Add breadcrumb

--- src/pages/BackofficeCategories.js
+++ src/pages/BackofficeCategories.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import CategoriesList from '../components/categories/CategoriesList';
-
+import { Breadcrumb } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 
 function BackofficeCategories(props) {
 
   return(
 
+    <>
+      <Breadcrumb className="mt-3 ms-3">
+        <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/" }}>Inicio</Breadcrumb.Item>
+        <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/backoffice" }}>Backoffice</Breadcrumb.Item>
+        <Breadcrumb.Item active>Categorias</Breadcrumb.Item>
+      </Breadcrumb>
       <CategoriesList/>
+    </>
 
     );
 
