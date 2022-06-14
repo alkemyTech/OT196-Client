c3de5ae962ac7b9d9cd2b0918c846e9cfd6d30a8
Facundo <facugastonbarral@gmail.com>
2022-06-13 22:41:18 -0300

    Create backoffice categories page

---
+++ src/pages/BackofficeCategories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CategoriesList from '../components/categories/CategoriesList';
+
+
+function BackofficeCategories(props) {
+
+  return(
+
+      <CategoriesList/>
+
+    );
+
+}
+
+export default BackofficeCategories;
