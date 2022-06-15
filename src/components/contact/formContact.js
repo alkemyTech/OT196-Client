75d6f126007ccccfe5005b77039f100477b5e2ae
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-06-15 15:56:09 -0300

    Added function that triggers when form is sent

--- src/components/contact/formContact.js
+++ src/components/contact/formContact.js
@@ -1,128 +1,163 @@
-import React, { useState } from 'react';
-import { ErrorMessage, Field, Form, Formik } from 'formik';
-import * as yup from 'yup';
-import 'bootstrap/dist/css/bootstrap.css';
-import {Spinner} from 'react-bootstrap';
+import React, { useState } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as yup from "yup";
+import "bootstrap/dist/css/bootstrap.css";
+import { Spinner } from "react-bootstrap";
+import { successAlert, errorAlert } from "../../setupAlerts";
 
-import { postRequest } from '../services/RequestService';
+import { postRequest } from "../../services/RequestService";
 
 export default function Formcontact() {
-  const [isLoading, setIsLoading] = useState({status: false, message: ''})
-  const {REACT_APP_BACKEND_URL} = process.env
-  const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+  const [isLoading, setIsLoading] = useState({ status: false, message: "" });
+  const { REACT_APP_BACKEND_URL } = process.env;
+  const phoneRegExp =
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
   const contactSchema = yup.object().shape({
-    name: yup.string()
-      .min(2, 'El nombre debe tener al menos 2 caracteres!')
-      .max(50, 'El nombre no puede tener más de 50 caracteres')
-      .required('Nombre es un campo obligatorio.'),
-    email: yup.string()
-      .email('Este email no es válido')
-      .required('Email es un campo obligatorio.')
-      .matches(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-      'El Email solo puede contener letras, numeros, puntos, guiones y guion bajo'),
-    message: yup.string()
-      .min(20, 'El mensaje debe contener al menos 20 caracteres')
-      .max(300, 'El mensaje no puede contener más de 300 caracteres')
-      .required('Mensaje es un campo obligatorio.'),
-    phone: yup.string().matches(phoneRegExp, 'El numero de telefono es invalido.')
-      .max(20, 'El telefono no puede contener más de 20 caracteres.')
-    });
-  return(
+    name: yup
+      .string()
+      .min(2, "El nombre debe tener al menos 2 caracteres!")
+      .max(50, "El nombre no puede tener más de 50 caracteres")
+      .required("Nombre es un campo obligatorio."),
+    email: yup
+      .string()
+      .email("Este email no es válido")
+      .required("Email es un campo obligatorio.")
+      .matches(
+        /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        "El Email solo puede contener letras, numeros, puntos, guiones y guion bajo"
+      ),
+    message: yup
+      .string()
+      .min(20, "El mensaje debe contener al menos 20 caracteres")
+      .max(300, "El mensaje no puede contener más de 300 caracteres")
+      .required("Mensaje es un campo obligatorio."),
+    phone: yup
+      .string()
+      .matches(phoneRegExp, "El numero de telefono es invalido.")
+      .max(20, "El telefono no puede contener más de 20 caracteres."),
+  });
+
+  const onSubmitForm = async (values, actions) => {
+    try {
+      setIsLoading({ status: true, message: "" });
+      await postRequest(`${REACT_APP_BACKEND_URL}/contacts/`, values);
+      actions.resetForm({
+        values: {
+          name: "",
+          email: "",
+          message: "",
+          phone: "",
+        },
+      });
+      setIsLoading({ status: false, message: "" });
+      // successAlert({});
+    } catch (e) {
+      setIsLoading({ status: false, message: "" });
+      console.error(e);
+      // errorAlert({});
+    }
+  };
+  return (
     <div>
       <Formik
-    
         initialValues={{
-          name: '',
-          email:'',
-          message: '',
-          phone: ''
+          name: "",
+          email: "",
+          message: "",
+          phone: "",
         }}
-
         //Validación del schema creado con yup
-        validationSchema= {contactSchema}
-
-        //Función para enviar los valores al endpoint(cuando este exista)
-        onSubmit={ async (values, actions) => {
-          try{
-            setIsLoading({status: true, message: ''})
-            await postRequest(`${REACT_APP_BACKEND_URL}/contacts/`, values)
-            actions.resetForm({
-              values: {
-                name: '',
-                email:'',
-                message: '',
-                phone: ''
-              }},
-              )
-            setIsLoading({status: false, message: 'Mensaje enviado con exito.'})
-            setTimeout(()=>setIsLoading({status: false, message: ''}), 5000)
-          }
-          catch(e){
-            setIsLoading({status: false, message: 'Error al enviar mensaje.'})
-          }
-        }}
-        >
-        {( {errors} ) => (
-			    <Form>
-				    <div className='form-inline'>
-					    <label className='fs-5' htmlFor="name">Nombre</label>
-					    <Field
-                className="form-control"
-					    	type="text"
-					     	name="name"
-					    	placeholder="Ej: Samuel Jackson"
-					    	id="name"
-					    />
-              <ErrorMessage name='name' component={() => (
-                <div className="alert alert-danger">{errors.name}</div>
-              )}/>
-				    </div>
-				    <div className='form-group'>
-					    <label className='fs-5' htmlFor="email">Email de contacto</label>
-					    <Field
-                className="form-control"
-					    	type="text"
-					    	name="email"
-					    	placeholder="Ej: Samuel_Jackson@gmail.com"
-					    	id="email"
-				  	  />
-              <ErrorMessage name='email' component={() => (
-                <div className="alert alert-danger">{errors.email}</div>
-              )}/>
-			  	  </div>
-				    <div className='form-group'>
-					    <label className='fs-5' htmlFor="phone">Telefono de contacto</label>
-					    <Field
-                className="form-control"
-					    	type="text"
-					    	name="phone"
-					    	placeholder="Ej: 113252185"
-					    	id="phone"
-				  	  />
-              <ErrorMessage name='phone' component={() => (
-                <div className="alert alert-danger">{errors.phone}</div>
-              )}/>
-			  	  </div>
-            <div className='form-group'>
-              <label className='fs-5'>Mensaje</label>
+        validationSchema={contactSchema}
+        onSubmit={onSubmitForm}
+      >
+        {({ errors }) => (
+          <Form>
+            <div className="form-inline">
+              <label className="fs-5" htmlFor="name">
+                Nombre
+              </label>
               <Field
                 className="form-control"
-                as='textarea'
+                type="text"
+                name="name"
+                placeholder="Ej: Samuel Jackson"
+                id="name"
+              />
+              <ErrorMessage
+                name="name"
+                component={() => (
+                  <div className="alert alert-danger">{errors.name}</div>
+                )}
+              />
+            </div>
+            <div className="form-group">
+              <label className="fs-5" htmlFor="email">
+                Email de contacto
+              </label>
+              <Field
+                className="form-control"
+                type="text"
+                name="email"
+                placeholder="Ej: Samuel_Jackson@gmail.com"
+                id="email"
+              />
+              <ErrorMessage
+                name="email"
+                component={() => (
+                  <div className="alert alert-danger">{errors.email}</div>
+                )}
+              />
+            </div>
+            <div className="form-group">
+              <label className="fs-5" htmlFor="phone">
+                Telefono de contacto
+              </label>
+              <Field
+                className="form-control"
+                type="text"
+                name="phone"
+                placeholder="Ej: 113252185"
+                id="phone"
+              />
+              <ErrorMessage
+                name="phone"
+                component={() => (
+                  <div className="alert alert-danger">{errors.phone}</div>
+                )}
+              />
+            </div>
+            <div className="form-group">
+              <label className="fs-5">Mensaje</label>
+              <Field
+                className="form-control"
+                as="textarea"
                 name="message"
                 placeholder="Escribe tu mensaje aqui"
                 id="message"
               />
-              <ErrorMessage name='message' component={() => (
-                <div className="alert alert-danger">{errors.message}</div>
-              )}/>
+              <ErrorMessage
+                name="message"
+                component={() => (
+                  <div className="alert alert-danger">{errors.message}</div>
+                )}
+              />
             </div>
-            <br/>
-				    <button className="btn ps-5 pe-5 mb-3 btn-outline-dark" type="submit">Enviar</button>
-            <div className='text-green'>
-            {(isLoading.status) ? <Spinner animation="border" variant="dark" /> : isLoading.message}
+            <br />
+            <button
+              className="btn ps-5 pe-5 mb-3 btn-outline-dark"
+              type="submit"
+            >
+              Enviar
+            </button>
+            <div className="text-green">
+              {isLoading.status ? (
+                <Spinner animation="border" variant="dark" />
+              ) : (
+                isLoading.message
+              )}
             </div>
-			    </Form>
+          </Form>
         )}
       </Formik>
     </div>
