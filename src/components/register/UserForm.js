c78e76b19b729e020d3f851b7dac828426a5f589
gerardodelascuevas <gerdlcp@gmail.com>
2022-06-15 17:20:05 -0500

    connecting correctly with the api API and  storing information in local storage

--- src/components/register/UserForm.js
+++ src/components/register/UserForm.js
@@ -1,10 +1,26 @@
-import React from "react"
+import React, { useEffect } from "react"
 import * as yup from "yup"
 import { Formik } from "formik"
 import { Button, Form } from "react-bootstrap"
+import axios from "axios"
+import Swal from "sweetalert2"
+import { useNavigate } from "react-router-dom"
 
 export default function UserForm() {
+  const navigate = useNavigate()
 
+  //FOR A FAST AUTHENTICATION, 
+  //IT IS CHECKED IF THERE IS A TOKEN OF THIS APP IN LOCAL STORAGE, 
+  //IF THERE IS A LOG IN IMMEDIATELY
+  useEffect(()=> {
+    const token = localStorage.getItem('somosMasToken')
+    if(token){
+      Swal.fire('Bienvenido de Nuevo')
+      .then(result=> {
+        if(result.isConfirmed) navigate('/')
+      })
+    }  
+  }, [])
   const initialValues = {
     firstName: '',
     lastName: '',
@@ -12,15 +28,36 @@ export default function UserForm() {
     password: ''
   }
 
-  const handleSubmit = (e) => {
+  const {REACT_APP_BACKEND} = process.env
+
+  const handleSubmit = async (e) => {
     const newUser = {
       firstName: e.firstName,
       lastName: e.lastName,
       email: e.email,
       password: e.password
     }
-    return newUser
+    //CREATE USER IN DATABASE 
+    const response = await axios.post(`${REACT_APP_BACKEND}/users/auth/register`, newUser)
+    //GET A NEW VALID TOKEN FOR OUR USER 
+    const token = await axios.post(`${REACT_APP_BACKEND}/jwt/auth/login`, response.data)
+    //PARSE TOKEN DATA TO JSON
+    const userToken = JSON.stringify(token.data.token)
+    //PERSIST TOKEN IN LOCAL STORAGE 
+    localStorage.setItem('somosMasToken', userToken)
+    Swal.fire({
+      icon: 'success', 
+      title: 'Usuario creado con exito'
+    }).then(result=> {
+      if(result.isConfirmed) navigate('/')
+    }).catch(()=> {
+      Swal.fire({
+        icon: 'error', 
+        title: 'Error en la petición HTTP'
+      })
+    })
   }
+
 
   const schema = yup.object({
     firstName: yup.string().max(16, 'Must be 16 characters or less').required('Required'),
