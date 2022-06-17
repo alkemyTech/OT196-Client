86059bb20af01da83a6333bdb6aae6aa7cc1ac18
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-06-17 15:10:22 -0300

    Fixed a bug where content dissapears on the edit form

--- src/components/news/NewsForm.js
+++ src/components/news/NewsForm.js
@@ -7,7 +7,7 @@ import { Form, Image } from "react-bootstrap";
 const NewsForm = ({ newsObject }) => {
   const [name, setName] = React.useState("");
   const [image, setImage] = React.useState("");
-  const [content, setContent] = React.useState("");
+  const [content, setContent] = React.useState();
   const [category, setCategory] = React.useState("Otros");
 
   const [method, setMethod] = React.useState("POST");
@@ -93,7 +93,7 @@ const NewsForm = ({ newsObject }) => {
           value={name}
         />
         <label className="sr-only">Contenido</label>
-        {content && (
+        {content !== undefined && (
           <CKEditor
             editor={ClassicEditor}
             data={content}
