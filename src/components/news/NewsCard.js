ea426877593d83dc7b7f7f83b6275f51e2251bf3
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-06-17 12:35:26 -0300

    Files modified to make the app work again

--- src/components/news/NewsCard.js
+++ src/components/news/NewsCard.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
 
 export default function NewsCard(props) {
-
-  const { img, title } = props.data
+  const { img, title } = props.newData;
 
   return (
     <Card className="border-0 col-sm-2">
       <Card.Img variant="top" src={img} className="img-fluid rounded" />
       <Card.Body>
-        <Card.Title className='text-center'>{title}</Card.Title>
+        <Card.Title className="text-center">{title}</Card.Title>
       </Card.Body>
     </Card>
-  )
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,66 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { Button, Row, Spinner } from "react-bootstrap";
 import { exampleNewsData } from "../components/news/exampleNewsData";
-import NewsCard from "../components/news/NewsCard";
+import NewsCard from "../components/news/NewsBox";
 import WelcomeTitle from "../components/home/WelcomeTitle";
 import SliderComponent from "../features/sliderComponent/SliderComponent";
-import { motion } from 'framer-motion'
-import { getRequest } from '../services/RequestService'
-import { Link } from 'react-router-dom'
+import { motion } from "framer-motion";
+import { getRequest } from "../services/RequestService";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const { REACT_APP_BACKEND_URL } = process.env
-  const [data, setData] = useState({})
-  const [isReady, setIsReady] = useState({status: false, message: 'Cargando información'})
+  const { REACT_APP_BACKEND_URL } = process.env;
+  const [data, setData] = useState({});
+  const [isReady, setIsReady] = useState({
+    status: false,
+    message: "Cargando información",
+  });
 
   useEffect(() => {
-    async function fetchLastNews(){
-      try{
-        const res = await getRequest(`${REACT_APP_BACKEND_URL}/news/`)
-        setData(res)
-        setIsReady({status: true, message: ''})
-      }
-      catch(e){
-        setIsReady({status: false, message: 'Error al obtener la información'})
+    async function fetchLastNews() {
+      try {
+        const res = await getRequest(`${REACT_APP_BACKEND_URL}/news/`);
+        setData(res);
+        setIsReady({ status: true, message: "" });
+      } catch (e) {
+        setIsReady({
+          status: false,
+          message: "Error al obtener la información",
+        });
       }
     }
-    fetchLastNews()
-  }, [REACT_APP_BACKEND_URL])
+    fetchLastNews();
+  }, [REACT_APP_BACKEND_URL]);
 
   return (
-    <motion.div className="container-fluid"
-    initial={{opacity: 0}}
-    animate={{opacity: 1}}
-    exit={{opacity: 0}}
+    <motion.div
+      className="container-fluid"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
       <WelcomeTitle text={data.welcome} />
-    <div className="container-fluid">
-      <WelcomeTitle text="Inicio" />
       <div className="container-fluid">
-        <SliderComponent />
-      </div>
-      <div className="news-container">
-        <div className="news-title my-4 py-4">
-
-          <h2>Ultimas novedades</h2>
+        <WelcomeTitle text="Inicio" />
+        <div className="container-fluid">
+          <SliderComponent />
         </div>
-        <Row className="px-3" xs={1} sm={2} md={2} xl={3}>
-          {isReady.status ?
-            ((data.slice(3)).reverse()).map((news, index) => {
-              return (<NewsCard newData={news} key={news.id}/>)
-            })
-            : 
-            <div className='mx-auto'>
-            <Spinner animation="border" variant="primary" />
-            <h5>{isReady.message}</h5>
-            </div>}
-        </Row>
-      </div>
-      <div className="all-news-button  my-4 py-4">
-        <Link to="/novedades">
-          <Button variant="outline-primary" >Ver más noticias</Button>
-        </Link>
-      </div>
+        <div className="news-container">
+          <div className="news-title my-4 py-4">
+            <h2>Ultimas novedades</h2>
+          </div>
+          <Row className="px-3" xs={1} sm={2} md={2} xl={3}>
+            {isReady.status ? (
+              data
+                .slice(3)
+                .reverse()
+                .map((news, index) => {
+                  return <NewsCard newData={news} key={news.id} />;
+                })
+            ) : (
+              <div className="mx-auto">
+                <Spinner animation="border" variant="primary" />
+                <h5>{isReady.message}</h5>
+              </div>
+            )}
+          </Row>
+        </div>
+        <div className="all-news-button  my-4 py-4">
+          <Link to="/novedades">
+            <Button variant="outline-primary">Ver más noticias</Button>
+          </Link>
+        </div>
       </div>
     </motion.div>
   );
