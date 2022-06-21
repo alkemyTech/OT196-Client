e978e7fa9628112b4c93ab3155ab02dee3ea7de2
Santiago Raab <Santiagoraab@gmail.com>
2022-06-21 15:43:54 -0300

    [OT196-93]Add list of members to screen contacts

---
+++ src/components/members/MemberItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {  Card, Row, Col } from "react-bootstrap";
+import imgDefault from './img/DefaultImage.png';
+
+export const MemberItem = ({ member }) => {
+    return(
+        <Card  style={{ width: '100%' }} >
+            <Row className="d-flex flex-nowrap align-items-center" >
+                <Col xs={4}>
+                    <img 
+                    className="ms-4 img-circle ratio ratio-1x1" 
+                    src={member.image || imgDefault } 
+                    style={{ borderRadius:'50%' ,maxHeight: '100px', maxWidth: '100px' }}
+                    />
+                </Col>
+                <Col xs={8}>
+                    <Card.Body className="d-flex flex-column">
+                        <Card.Title className="fs-5">{member.name}</Card.Title>
+                    </Card.Body>
+                </Col>
+            </Row>
+        </Card>
+    )
+}
