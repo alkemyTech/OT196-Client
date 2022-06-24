9a37777cb461ef36e8bdc3ff48758e0b3bae74a9
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-06-24 17:06:56 -0300

    Added motion transitions

--- src/pages/NoMatchRoute.jsx
+++ src/pages/NoMatchRoute.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Button } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom';
-import {motion} from 'framer-motion'
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import customTransition from "../components/utils/CustomTransition";
 
-export default function NoMatchRoute(){
-    const navigate = useNavigate()
-   
-    return(
-        <motion.div
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        exit={{opacity: 0}}
-        > 
-            <h1> La ruta que estas buscando no existe en nuestra app </h1>
+export default function NoMatchRoute() {
+  const navigate = useNavigate();
 
-            <h6> Para volver a home da click en el botón </h6>
-            <Button variant="primary" size="lg" onClick={()=> navigate('/')}>
-                Volver al Inicio
-            </Button>
-        </motion.div>
-    )
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={customTransition}
+    >
+      <h1> La ruta que estas buscando no existe en nuestra app </h1>
+
+      <h6> Para volver a home da click en el botón </h6>
+      <Button variant="primary" size="lg" onClick={() => navigate("/")}>
+        Volver al Inicio
+      </Button>
+    </motion.div>
+  );
 }
