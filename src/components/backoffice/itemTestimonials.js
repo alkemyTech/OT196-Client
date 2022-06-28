d122ec28b379f129e4bb08ff69340ba58142ca96
Santiago Raab <Santiagoraab@gmail.com>
2022-06-28 17:08:23 -0300

    edit testimonials modal

--- src/components/backoffice/itemTestimonials.js
+++ src/components/backoffice/itemTestimonials.js
@@ -1,29 +1,21 @@
 import React from "react";
-import { FaRegEdit } from "react-icons/fa";
 import BtnDelete from "../utils/BtnDelete";
-import { Button } from "react-bootstrap";
+import EditTestimony from "../testimonials/TestimonyEdit";
 
 const TestimonyItem = ({ testimony, axiosApi }) => {
   const { REACT_APP_BACKEND_TESTIMONIALS } = process.env;
   const url = `${REACT_APP_BACKEND_TESTIMONIALS}`;
-  const testimonyContent = testimony.content;
-  
-  const onEdit = () => {
-    // setEdit(!edit)
-  };
 
   return (
-    <ul className="list-group-item bg-light rounded border">
+    <ul className="list-group-item m-1 bg-light rounded border">
       <div className="mt-2 d-flex justify-content-start">
         <strong>{testimony.name}</strong>
       </div>
       <div  className="m-2 d-flex justify-content-center">
-        <p dangerouslySetInnerHTML={{ __html: testimonyContent }}/>
+        <p dangerouslySetInnerHTML={{ __html: testimony.content }}/>
       </div>
       <div className="d-flex justify-content-end">
-        <Button className="me-2" onClick={() => onEdit()}>
-          Editar <FaRegEdit />
-        </Button>
+        <EditTestimony testimony={testimony} axiosApi={axiosApi}/>
         <BtnDelete
           apiRoute={url}
           id={testimony.id}
