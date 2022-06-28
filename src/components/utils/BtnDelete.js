5b29b2701863bb06cf50386ee918f1895202e97f
Facundo <facugastonbarral@gmail.com>
2022-06-28 18:34:26 -0300

    Using request from service instead axios

--- src/components/utils/BtnDelete.js
+++ src/components/utils/BtnDelete.js
@@ -3,6 +3,8 @@ import { Button } from "react-bootstrap";
 import { FaTrashAlt } from "react-icons/fa";
 import { warningAlert, successAlert } from "../../setupAlerts";
 import axios from "axios";
+import { deleteRequest } from "../../services/RequestService";
+
 
 const BtnDelete = ({
   btnLabel = "Eliminar",
@@ -14,7 +16,7 @@ const BtnDelete = ({
   const triggerDelete = async (result, id) => {
     try {
       if (result.isConfirmed) {
-        await axios.delete(`${apiRoute}/${id}`);
+        await deleteRequest(`${apiRoute}/${id}`);
         successAlert({});
         // execute every function on the array
         for (let i = 0; i < arrFunc.length; i++) {
