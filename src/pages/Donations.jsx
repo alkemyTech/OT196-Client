5a10987291bd3af086ae1b0bc2371482ba7cbcfb
Facundo <facugastonbarral@gmail.com>
2022-06-29 15:10:20 -0300

    Better style

--- src/pages/Donations.jsx
+++ src/pages/Donations.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import StripeCheckout from 'react-stripe-checkout';
 import { useNavigate } from 'react-router-dom'
-import { Button } from 'react-bootstrap';
+import { Button, InputGroup, FormControl, Container } from 'react-bootstrap';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { FaDollarSign, FaHandsHelping } from "react-icons/fa"
 
 export default function Donations(){
   const navigate = useNavigate()
@@ -32,20 +33,31 @@ useEffect(()=> {
 }, [stripeToken])
 
   return (
-    <div>
-  <h1> Gracias por tus donaciones </h1>
+  <div>
+  <h1 className="mb-3"> Gracias por tus donaciones </h1>
   <h4> Con ella le permites a Somos Más llegar a mas personas </h4>
 
-  <input type='number' placeholder='Coloca tu donación' onChange={e=> handleChangeDonations(e)}/>
+  <Container className="w-50 mx-auto mb-3">
+    <InputGroup className="mb-3">
+      <InputGroup.Text><FaDollarSign/></InputGroup.Text>
+      <FormControl onKeyPress={(event) => {
+        if (!/[0-9]/.test(event.key)) {
+          event.preventDefault();
+        }
+      }} aria-label="Valor monetario" />
+    </InputGroup>
   <StripeCheckout
     token={onToken}
     stripeKey='pk_test_51KgvfFHEEv1tXsVZEpffi9X0VHCJ2XpgfPxentUc7Hx1qiTrS3uR1vXz0KDqpDkKBI5YX8ZLhxqah7FJhqK2vKXC00G70EZnc4'                      
     billingAddress                            
     description={`Tu donación: ${donation}`}
     amount={donation}
-  
     >   
-    <Button> Envia tu  Donación </Button>                       
+    <div className="d-grid gap-2">
+      <Button> <FaHandsHelping/> Envia tu  Donación </Button>       
+    </div>                
   </StripeCheckout>
+  </Container>
+
 </div>
 )}
